e5efac0f8ebc5d9a6b1d8c17730a6901717a97ba
damian-anslik <damiananslik@outlook.com>
2024-01-30 22:46:07 +0000

    handle article download exception

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import validators
 import newspaper
+import datetime
 import supabase
+import requests
 import openai
+import nltk
+import json
 
 
 def generate_summary(article_text: str, model_config: dict) -> str:
@@ -49,8 +53,48 @@ def get_article(url: str, app_config: dict) -> dict:
             "url": url,
             "summary": article_summary,
         }
+        if app_config["GET_RELATED_ARTICLES"]:
+            # Figure out how to best create a list of keywords from the article so we can get relevant related articles
+            nltk.download("punkt")
+            article.nlp()
+            article_keywords = article.keywords
+            # article_keywords = article.title.split(" ")
+            if not article.publish_date:
+                article.publish_date = datetime.datetime.now()
+            related_articles = get_related_articles(
+                article_keywords=article_keywords,
+                from_date=article.publish_date,
+                api_key=app_config["NEWSAPI_API_KEY"],
+            )
+            article_data["related_articles"] = related_articles
         articles.insert(article_data).execute()
     return article_data
+
+
+def get_related_articles(
+    article_keywords: list[str],
+    from_date: datetime.datetime,
+    api_key: str,
+    limit: int = 5,
+) -> list[dict]:
+    query_string = " ".join(article_keywords)
+    from_date_string = from_date.date().strftime("%Y-%m-%d")
+    response = requests.get(
+        url="https://newsapi.org/v2/everything",
+        params={
+            "q": query_string,
+            "from": from_date_string,
+            "pageSize": limit,
+            "page": 1,
+        },
+        headers={"X-Api-Key": api_key},
+    )
+    if not response.ok:
+        raise Exception(f"Failed to get related articles: {response.text}")
+    response_data = response.json()
+    with open("related_articles.json", "w") as f:
+        json.dump(response_data, f)
+    return response_data["articles"]
 
 
 def add_chat_message(role: str, content: str):
@@ -63,6 +107,8 @@ def add_article(article_data: dict):
 
 app_config = {
     "APP_NAME": "TLDR News",
+    "GET_RELATED_ARTICLES": True,
+    "NEWSAPI_API_KEY": st.secrets["NEWSAPI_API_KEY"],
     "CHAT_INPUT_PLACEHOLDER": "Enter a URL to summarise...",
     "OPENAI_API_KEY": st.secrets["OPENAI_API_KEY"],
     "OPENAI_COMPLETIONS_MODEL": st.secrets["OPENAI_COMPLETIONS_MODEL"],
@@ -83,7 +129,14 @@ if url := st.chat_input(app_config["CHAT_INPUT_PLACEHOLDER"]):
         add_chat_message("assistant", "Please enter a valid URL.")
         st.rerun()
     with st.spinner(f"Getting article summary for '{url}'..."):
-        article_data = get_article(url, app_config)
+        try:
+            article_data = get_article(url, app_config)
+        except:
+            add_chat_message(
+                "assistant",
+                "An error occurred while trying to get the article summary. Please try again later.",
+            )
+            st.rerun()
     article_summary = article_data["summary"]
     article_title = article_data["title"]
     add_article(article_data)
